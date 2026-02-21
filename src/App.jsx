4117891522ea8a82262b5f71c9fd52ce86ab4630
Safa Ben Haj Ali <benhajalisafa5@gmail.com>
2026-02-21 22:40:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UseEffectBasics from './tutorial/02-useEffect/final/02-useEffect-basics';
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced React – 02 useEffect</h2>
+      <UseEffectBasics />
+    </div>
+  );
+}
+
+export default App;
